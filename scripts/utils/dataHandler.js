c1dcc6342fbf571fbbf6b5bb85775ec665e25a5e
julien382 <julhermain@gmail.com>
2022-05-12 21:47:31 +0200

    visio

--- scripts/utils/dataHandler.js
+++ scripts/utils/dataHandler.js
@@ -1,4 +1,20 @@
+export const getData = async () => {
+    const response = await fetch('../../data/photographers.json');
+
+    if (response.ok) {
+        const data = await response.json()
+        return data;
+    } else {
+        console.error('ça va pas:', response.status, response.statusText);
+    }
+}
+
 export const getPhotographers = async () => {
     const DATA = await getData()
     return DATA.photographers
+}
+
+export const getMedias = async () => {
+    const DATA = await getData()
+    return DATA.media
 }
