cd99dbe853d836d450c18b60d554accc7b6cbe15
julien382 <julhermain@gmail.com>
2022-05-19 00:40:40 +0200

    css photographers header ok sauf button contact

--- scripts/factories/pagePhotographers.js
+++ scripts/factories/pagePhotographers.js
@@ -10,6 +10,7 @@ export const indexPhotographerFactory = (data) => {
 
         const userCardDom = document.createElement( 'article' );
         const div = document.createElement( 'div' );
+        const divText = document.createElement( 'div' );
         const img = document.createElement( 'img' );
         const h1 = document.createElement( 'h1' );
         const pCity = document.createElement( 'p' );
@@ -17,20 +18,22 @@ export const indexPhotographerFactory = (data) => {
 
         // on remplie nos balises
         
-        userCardDom.setAttribute("class", "pp_photographer")
+        userCardDom.setAttribute("class", "pp_photographers")
         h1.textContent = name;
         pCity.textContent = city + ", " + country;
-        pCity.setAttribute("class", "pCity__pp_photographer")
+        pCity.setAttribute("class", "pCity__pp_photographers")
         pTagline.textContent = tagline;
 
-        div.setAttribute("class", "img_pp_photographer")
+        div.setAttribute("class", "img_pp_photographers")
+        divText.setAttribute("class", "text_pp_photographers")
         img.setAttribute("src", picture)
 
         userCardDom.appendChild(div);
-        userCardDom.appendChild(img);
-        userCardDom.appendChild(h1);
-        userCardDom.appendChild(pCity);
-        userCardDom.appendChild(pTagline);
+        userCardDom.appendChild(divText);
+        div.appendChild(img);
+        divText.appendChild(h1);
+        divText.appendChild(pCity);
+        divText.appendChild(pTagline);
 
         // on retourne la balise article
         return userCardDom;
