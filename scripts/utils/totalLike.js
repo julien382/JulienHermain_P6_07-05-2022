a16b0eff28b3282b4a680d1ab6a636418a81d3a5
julien382 <julhermain@gmail.com>
2022-06-10 16:01:47 +0200

    totalLike ajout texte

--- scripts/utils/totalLike.js
+++ scripts/utils/totalLike.js
@@ -15,11 +15,19 @@ export const displayMediaLike = async (medias) => {
 const createCard = (media, totalLike) => {
 
     const card = document.createElement('div');
-    card.classList.add('card')
+    card.classList.add('totalPriceMonth')
     console.log(totalLike);
     const pLike = document.createElement('p')
+    pLike.setAttribute("class", "pNumberLike")
     pLike.innerText = totalLike
     card.append(pLike)
+    //const priceMonth = document.createElement('p')
+    //priceMonth.innerText = photographers.name.price
+    //card.append(priceMonth)
+    const textPriceM = document.createElement('p')
+    textPriceM.innerText = "€ /jour"
+    console.log(textPriceM);
+    card.append(textPriceM)
     
     return card
 };
