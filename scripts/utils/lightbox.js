4546967e27f653bc2cf4002eae51430335a3f039
julien382 <julhermain@gmail.com>
2022-06-16 13:50:49 +0200

    save visio

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -7,26 +7,31 @@ export class Lightbox {
   constructor(url, images) {
     this.element = this.buildDOM(url)
     this.images = images
-    this.loadImage(url)
+    this.loadMedia(url)
     this.onKeyUp = this.onKeyUp.bind(this)
     document.body.appendChild(this.element)
     document.addEventListener('keyup', this.onKeyUp)
   }
 
-  loadImage (url) {
+  loadMedia (url) {
     this.url = null
-    const image = new Image()
+
     const container = this.element.querySelector('.lightbox_image')
-    const loader = document.createElement('div')
-    loader.classList.add('lightbox__loader')
     container.innerHTML = ''
-    container.appendChild(loader)
-    image.onload = () => {
-      container.removeChild(loader)
+    
+    const mp4Regex = /\.mp4$/
+    if (!mp4Regex.test(url)) {
+      const image = new Image()
       container.appendChild(image)
       this.url = url
+      image.src = url
+    } else {
+      const video = document.createElement('video')
+      video.controls = true
+      container.appendChild(video)
+      this.url = url
+      video.src = url
     }
-    image.src = url
   }
 
   onKeyUp (e) {
@@ -55,7 +60,7 @@ export class Lightbox {
     if (i == this.images.length - 1) {
       i = -1
     }
-    this.loadImage(this.images[i + 1])
+    this.loadMedia(this.images[i + 1])
   }
 
   prev (e) {
@@ -64,53 +69,49 @@ export class Lightbox {
     if (i == 0) {
       i = this.images.length
     }
-    this.loadImage(this.images[i - 1])
+    this.loadMedia(this.images[i - 1])
   }
 
   buildDOM(url) {
-    // regex .mp4 ?
-    // const mp4Regex = /^\w+.(mp4)$/ ==> regex .mp4
     const dom = document.createElement('div')
     dom.classList.add('lightbox')
-    dom.innerHTML = `
-    <div class="lightbox__container_all">
-      <button class="lightbox__close lightbox_close"></button>
-      <button class="lightbox__next"></button>
-      <button class="lightbox__prev"></button>
-      <div class="lightbox__container">
-        <div class="lightbox_image">
-          <img src="${url}" alt="">
+
+    const mp4Regex = /\.mp4$/
+
+    if (!mp4Regex.test(url)) {
+      dom.innerHTML = `
+      <div class="lightbox__container_all">
+        <button class="lightbox__close lightbox_close"></button>
+        <button class="lightbox__next"></button>
+        <button class="lightbox__prev"></button>
+        <div class="lightbox__container">
+          <div class="lightbox_image">
+            <img src="${url}" alt="">
+          </div>
+          <div class="lightbox_text">
+            <p class="title_image">Arc-en-ciel</p>
+          </div>
         </div>
-        <div class="lightbox_text">
-          <p class="title_image">Arc-en-ciel</p>
+      </div>`
+    } else {
+      dom.innerHTML = `
+      <div class="lightbox__container_all">
+        <button class="lightbox__close lightbox_close"></button>
+        <button class="lightbox__next"></button>
+        <button class="lightbox__prev"></button>
+        <div class="lightbox__container">
+          <div class="lightbox_image">
+            <video src="${url}">
+          </div>
+          <div class="lightbox_text">
+            <p class="title_image">Arc-en-ciel</p>
+          </div>
         </div>
-      </div>
-    </div>`
-    /*dom.innerHTML = `
-      <button class="lightbox__close lightbox_close"></button>
-      <button class="lightbox__next"></button>
-      <button class="lightbox__prev"></button>
-      <div class="lightbox__container"></div>`*/
+      </div>`
+    }
     dom.querySelector('.lightbox__close').addEventListener('click', this.close.bind(this))
     dom.querySelector('.lightbox__next').addEventListener('click', this.next.bind(this))
     dom.querySelector('.lightbox__prev').addEventListener('click', this.prev.bind(this))
     return dom
   }
 }
-/*
-<div id="lightbox_modal" class="lightbox">
-      <div class="lightbox__container_all">
-        <div class="lightbox__container">
-              <button class="lightbox__close lightbox_close"></button>
-              <button class="lightbox__next"></button>
-              <button class="lightbox__prev"></button>
-              <div class="lightbox_image">
-                <img  src="/assets/photographers/MimiKeel.jpg" alt="">
-              </div>
-              <div class="lightbox_text">
-                <p class="title_image">Arc-en-ciel</p>
-              </div>
-        </div>
-      </div>
-    </div>
-    */
--- scripts/utils/likesHandler.js
+++ scripts/utils/likesHandler.js
@@ -1,17 +1,19 @@
 export const likesHandler = () => {
     const pNumberLike = document.querySelector('.pNumberLike')
-    const heartImage = document.querySelector('.heartImage')
+    const heartImage = document.querySelectorAll('.heartImage')
 
     heartImage.forEach(div => {
         div.addEventListener('click', () => {
             const parent = div.parentElement
             const nombreLikes = parent.querySelector('.nombreLikes')
 
+            // si il avait déjà cliqué
             if (div.classList.contains('liked')) {
                 const newTotalLikes = parseInt(nombreLikes.innerHTML) - 1
                 const newGlobalLikes = parseInt(pNumberLike.innerHTML) - 1
                 nombreLikes.innerHTML = newTotalLikes
                 pNumberLike.innerHTML = newGlobalLikes
+
             } else {
                 const newTotalLikes = parseInt(nombreLikes.innerHTML) + 1
                 const newGlobalLikes = parseInt(pNumberLike.innerHTML) + 1
@@ -20,6 +22,7 @@ export const likesHandler = () => {
             }
 
             div.classList.toggle('liked')
+
         })
     })
 }
