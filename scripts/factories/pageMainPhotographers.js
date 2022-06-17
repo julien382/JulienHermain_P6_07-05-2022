e7422a7f6074ddc9466d7641cfb808a197a313a2
julien382 <julhermain@gmail.com>
2022-06-17 15:58:22 +0200

    coeur ok pour video

--- scripts/factories/pageMainPhotographers.js
+++ scripts/factories/pageMainPhotographers.js
@@ -1,14 +1,10 @@
 export const displayMedia = async (medias, photographerName) => {
     const pictures = document.querySelector(".pictures");
 
-    // Mimi keel ==> Mimi
-    const firstName = photographerName.split(' ')[0]
-    const cleanFirstName = firstName.replace("-", " ");
-
-    let totalLike = 0
-
+    const firstName = photographerName.split(' ')[0];
+    const cleanFirstName = firstName.replace("-", " "); // Enlève les tirets du prénom
+    
     medias.forEach(media => {
-        totalLike += media.likes
         // const mediaPath = est-ce que true ? si true : si false
         const mediaPath = media.image ? media.image : media.video
         // const mediaPath = media.image ?? media.video
@@ -17,7 +13,6 @@ export const displayMedia = async (medias, photographerName) => {
         const card = createCard(path, media)
         pictures.append(card)
     });
-
 };
 
 const createCard = (path, media) => {
@@ -30,30 +25,38 @@ const createCard = (path, media) => {
         const a = document.createElement('a')
         a.setAttribute("href", path)
         a.classList.add('linkLightbox'); 
+
         const divImage = document.createElement('div')
         divImage.setAttribute("class", "divImage")
+
         const img = document.createElement('img')
         img.setAttribute("class", "imagesQuadrillage")
         img.src = path
+
         const divText = document.createElement('div')
         divText.setAttribute("class", "divTextImage")
+
         const title = document.createElement('h2')
         title.setAttribute("class", "titleImage")
         title.innerText = media.title
+
         const divLikes = document.createElement('div')
+        // divLikes.classList.add('divLikes')
         divLikes.setAttribute("class", "divLikes")
+
         const nLikes = document.createElement('p')
         nLikes.setAttribute("class", "nombreLikes")
         nLikes.innerText = media.likes
+
         const heart = document.createElement('div')
         heart.setAttribute("class", "heartImage")
 
         const full = document.createElement('img')
         full.classList.add('full')
-        full.src = "../../assets/icons/heart.svg"
+        full.src = '../../assets/icons/heart.svg'
         const empty = document.createElement('img')
         empty.classList.add('empty')
-        empty.src = "../../assets/icons/heart_empty.svg"
+        empty.src = '../../assets/icons/heart_empty.svg'
 
         heart.append(full)
         heart.append(empty)
@@ -86,8 +89,19 @@ const createCard = (path, media) => {
         const nLikes = document.createElement('p')
         nLikes.setAttribute("class", "nombreLikes")
         nLikes.innerText = media.likes
-        const heart = document.createElement('img')
+        
+        const heart = document.createElement('div')
         heart.setAttribute("class", "heartImage")
+
+        const full = document.createElement('img')
+        full.classList.add('full')
+        full.src = '../../assets/icons/heart.svg'
+        const empty = document.createElement('img')
+        empty.classList.add('empty')
+        empty.src = '../../assets/icons/heart_empty.svg'
+
+        heart.append(full)
+        heart.append(empty)
 
         card.append(a)
         a.append(divImage)
@@ -101,26 +115,3 @@ const createCard = (path, media) => {
 
     return card
 };
-
-/////////////////**/
-/*
-export const displayMediaName = async (photographerName) => {
-    const name = document.querySelector(".name_photographer_section");
-console.log(photographerName)
-    name.append(card)
-    // Mimi keel ==> Mimi
-
-    
-    //const card = createName(photographerName)
-    //pictures.append(card)
-    
-};
-
-const createName = (path) => {
-    const card = document.createElement('div')
-    card.classList.add('p')
-
-    card.append(p)
-
-    return card
-};*/
